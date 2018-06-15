821b7ea027c53b054cd0283ae55090ead2577f73
Vadimka Komissarov <vadimka_kom@mail.ru>
2018-06-15 04:21:43 +0300

    added migrations for the last commit

---
+++ extras/migrations/1529025553_host_fk_for_jobs.up.sql
@@ -0,0 +1,22 @@
+-- MySQL Workbench Synchronization
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+ALTER TABLE `ks-installer`.`hosts` 
+CHANGE COLUMN `hostname` `hostname` VARCHAR(64) NOT NULL ,
+ADD COLUMN `created_by` VARCHAR(36) NOT NULL AFTER `ipmi_address`,
+ADD INDEX `fk_hosts_created_by_idx` (`created_by` ASC);
+
+ALTER TABLE `ks-installer`.`hosts` 
+ADD CONSTRAINT `fk_hosts_created_by`
+  FOREIGN KEY (`created_by`)
+  REFERENCES `ks-installer`.`jobs` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
